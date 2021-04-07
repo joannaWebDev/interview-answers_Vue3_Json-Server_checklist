ed1f25143b804dc7beeedadf9a6da9d7ea185a00
unknown <joannasmerea@gmail.com>
2021-04-07 10:15:19 +0200

    quick answers checklist with Vue3

---
+++ README.md
@@ -0,0 +1,38 @@
+# Interview checklist Vue3   
+--- 
+
+Here I answer some of the more general questions that you as a recruiter might have for me.  
+
+## Quick peek  
+
+<p>
+  <img src="Vue3_interview-answers_checklist.gif" alt="Vue3_interview-answers_checklist"/>
+</p>
+
+
+
+## Project setup
+
+```
+npm install
+```
+
+## Run the JSON-Server backend (http://localhost:5000)
+
+```
+npm run backend
+```
+
+## Run the Vue dev server (http://localhost:8080)
+
+```
+npm run serve
+```
+
+## Compiles and minifies for production
+
+```
+npm run build
+```
+
+
---
+++ src/components/AddTask.vue
@@ -0,0 +1,96 @@
+<template>
+  <form @submit="onSubmit" class="add-form">
+    <div class="form-control">
+      <label>Task</label>
+      <input type="text" v-model="text" name="text" placeholder="Add Task" />
+    </div>
+    <div class="form-control">
+      <label>answer</label>
+      <input
+        type="text"
+        v-model="answer"
+        name="answer"
+        placeholder="Add answer"
+      />
+    </div>
+    <div class="form-control form-control-check">
+      <label>Set Reminder</label>
+      <input type="checkbox" v-model="reminder" name="reminder" />
+    </div>
+
+    <input type="submit" value="Save Task" class="btn btn-block" />
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'AddTask',
+  data() {
+    return {
+      text: '',
+      answer: '',
+      reminder: false,
+    }
+  },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault()
+
+      if (!this.text) {
+        alert('Please add a task')
+        return
+      }
+
+      const newTask = {
+        // id: Math.floor(Math.random() * 100000),
+        text: this.text,
+        answer: this.answer,
+        reminder: this.reminder,
+      }
+
+      this.$emit('add-task', newTask)
+
+      this.text = ''
+      this.answer = ''
+      this.reminder = false
+    },
+  },
+}
+</script>
+
+<style scoped>
+.add-form {
+  margin-bottom: 40px;
+}
+
+.form-control {
+  margin: 20px 0;
+}
+
+.form-control label {
+  display: block;
+}
+
+.form-control input {
+  width: 100%;
+  height: 40px;
+  margin: 5px;
+  padding: 3px 7px;
+  font-size: 17px;
+}
+
+.form-control-check {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.form-control-check label {
+  flex: 1;
+}
+
+.form-control-check input {
+  flex: 2;
+  height: 20px;
+}
+</style>
